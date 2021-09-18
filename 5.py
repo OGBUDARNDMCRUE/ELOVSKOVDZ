419e68b8c1d6f1a25ea72088cbb66152f4e59d36
OGBUDARNDMCRUE <demix3231@yandex.ru>
2021-09-18 08:28:48 +0300

    main 1.1

--- 5.py
+++ 5.py
@@ -2,11 +2,11 @@ x = int(input('Введите x:'))
 y = int(input('Введите y:'))
 if x>0 and y>0:
     print('Первая четверть')
-if x<0 and y>0:
+elif x < 0 and y > 0:
     print('Вторая четверть')
-if x<0 and y<0:
+elif x < 0 and y < 0:
     print('Третья четверть')
-if x>0 and y<0:
+elif x > 0 and y < 0:
     print('Четвертая четверть')
-if x == 0 or y == 0:
+elif x == 0 or y == 0:
     print('Точка лежит на оси')

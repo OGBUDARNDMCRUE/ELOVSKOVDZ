35af2665026bc0847db59ce0c6a0b8ab9e44e468
OGBUDARNDMCRUE <demix3231@yandex.ru>
2021-09-18 08:55:31 +0300

    main 2.0

--- 3.py
+++ 3.py
@@ -1,6 +1,6 @@
 from math import sqrt
-x = int(input('Введите x'))
-y = int(input('Введите y'))
-z = int(input('Введите z'))
-k = sqrt(x**2 + y**2 + z**2)
+x = int(input('Введите x:'))
+y = int(input('Введите y:'))
+z = int(input('Введите z:'))
+k = sqrt(x ** 2 + y ** 2 + z ** 2)
 print(k)

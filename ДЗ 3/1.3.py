d7956f6330b4c092135f40fc1706267ec4d37514
ElovskovRNB <90063704+OGBUDARNDMCRUE@users.noreply.github.com>
2021-11-23 01:30:22 +0300

    Delete ДЗ 3 directory

--- ДЗ 3/1.3.py
+++
@@ -1,9 +0,0 @@
-def func():
-    x = input('Введите что - нибудь:')
-    y = list()
-    while x != '':
-        y.append(x)
-        x = input('Введите ещё что - нибудь:')
-    return y 
-
-print(func())

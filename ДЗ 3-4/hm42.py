18bfc5d7d61df8d6029cdf27c5b418333e0c0d73
OGBUDARNDMCRUE <demix3231@yandex.ru>
2021-11-23 01:49:35 +0300

    1

--- ДЗ 3-4/hm42.py
+++
@@ -1,9 +0,0 @@
-def factorial(a):
-    x = 1
-    for i in range(2, a + 1):
-        x *= i
-    return x
-a = int(input("Введите число:"))
-print(factorial(a))
-assert factorial(5) == 120
-assert factorial(6) == 720
